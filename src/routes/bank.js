60eb54b74bdb710e7b29c935017bdec8ba25a766
SimrithaReddy <95024988+SimrithaReddy@users.noreply.github.com>
2022-12-19 01:00:02 +0530

    Update bank.js

--- src/routes/bank.js
+++ src/routes/bank.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Details = require("../models/user")
+const Transcations = require("../models/transcation")
 const bodyParser = require('body-parser')
 
 // Your routing code goes here
@@ -10,6 +11,16 @@ router.get('/', async (req, res) => {
     try {
         let details = await Details.find().sort({ _id: -1 })
         res.json(details)
+    } catch (e) {
+        res.sendStatus(404).json(e.message)
+    }
+})
+
+
+router.get('/details', async (req, res) => {
+    try {
+        let history = await Transcations.find().sort({ _id: -1 })
+        res.json(history)
     } catch (e) {
         res.sendStatus(404).json(e.message)
     }
@@ -31,29 +42,46 @@ router.post('/users', async (req, res) => {
 })
 
 
+
 router.post('/transfers', async (req, res) => {
-    try {
-      let send_ac = await Details.find({ account: req.body.sender })
-       let receive_ac = await Details.find({ account: req.body.receiver })
-     
-        if(send_ac[0].account === receive_ac[0].account){
-            return res.json("ok")
+    try {  
+
+      async function main() {
+            const createHistory = await Transcations.create({
+                sender: req.body.sender,
+                receiver: req.body.receiver,
+                amount: req.body.amount
+            })
         }
-        
+
+        let send_ac = await Details.find({ account: req.body.sender })      
+        let receive_ac = await Details.find({ account: req.body.receiver }) 
+
         let num1 = send_ac[0].balance
         let num2 = receive_ac[0].balance
         num1 = parseFloat(num1)
         num2 = parseFloat(num2)
         let amount = parseFloat(req.body.amount)
+
+        if(send_ac[0].account === receive_ac[0].account){
+            num1 = num1 + amount
+            let x = await Details.updateOne({ account: req.body.sender }, { $set: { balance: num1 } })
+            main();
+            return res.json(x)
+        }
+
+        if(amount<0 || amount > num1) return res.json(0)
+        
         
         if(send_ac.length!=0 && receive_ac.length != 0){
-        if (amount > 0 && num1 > 0) {
+        if (amount > 0 && num1 > 0 && (amount < num1 || amount == num1 )) {
+            main();
             num1 = num1 - amount
             num2 = num2 + amount
 
             let x = await Details.updateOne({ account: req.body.sender }, { $set: { balance: num1 } })
             let y = await Details.updateOne({ account: req.body.receiver }, { $set: { balance: num2 } })
-            res.json(num1)
+           return res.json("ok")
         }
         }
 
